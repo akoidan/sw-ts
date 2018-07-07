18843d54d645c42e78a434fcdf25fd5a94137b7a
deathangel908 <deathangel908@gmail.com>
2018-07-08 02:11:31 +0300

    removed redundant code

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,5 @@
 import './sw.ts'
+async function b() {
+  console.log('async b works inside main.ts');
+}
 console.log('main.ts');
--- src/sw.ts
+++ src/sw.ts
@@ -1,7 +1,4 @@
 async function a() {
   console.log('sw a called');
-  return 123;
 }
-
-
 console.log('sw');
