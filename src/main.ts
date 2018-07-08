1a59842ec252eb5ea8997c6b151ed68f5c6d5666
deathangel908 <deathangel908@gmail.com>
2018-07-08 03:21:45 +0300

    Simplified

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,2 @@
 import './sw.ts'
-async function b() {
-  console.log('async b works inside main.ts');
-}
 console.log('main.ts');
--- src/sw.ts
+++ src/sw.ts
@@ -1,11 +1,3 @@
-import {Logger, LoggerFactory, LogStrict} from 'lines-logger';
-
-declare var clients: any;
-
+import {LoggerFactory, LogStrict} from 'lines-logger';
+console.log('sw file');
 let loggerFactory: LoggerFactory = new LoggerFactory(LogStrict.LOG_WITH_WARNINGS);
-let logger: Logger = loggerFactory.getLoggerColor('SW_S', 'color: #ffb500; font-weight: bold');
-
-async function a() {
-  console.log('sw a called');
-}
-console.log('sw');
